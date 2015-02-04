8196b11ca7cb252cfa6fcd64949ec58b8cc891d0
Alex Yancey <alexyancey3@gmail.com>
2015-02-04 08:48:27 -0800

    End of class commit (also pretty much done)

--- scan.py
+++ scan.py
@@ -1,42 +1,56 @@
 import os
 import time
 
+
 class DirectoryMonitor:
 	directory = ''
 	interval = 0
 	on_added = None
 	on_removed = None
+	loop = 1
 
 	def __init__(self, path_i, interval_i):
 		self.directory = path_i
 		self.interval = interval_i
 
 	def start(self):
-		before = os.listdir(self.directory)
+		self.loop = 1
+		while self.loop == 1:
+			before = os.listdir(self.directory)
 
-		time.sleep(self.interval)
+			time.sleep(self.interval)
 
-		after = os.listdir(self.directory)
+			after = os.listdir(self.directory)
 
-		for file in before:
-			pass
-			#`print file
+			for file in before:
+				pass
+				#`print file
 
-		removed = list(set(before) - set(after))
-		for file in removed:
-			self.on_removed(file)
+			removed = list(set(before) - set(after))
+			for file in removed:
+				if not self.on_removed == None:
+					self.on_removed(file)
 
-		added = list(set(after) - set(before))
-		for file in added:
-			self.on_added(file)
+			added = list(set(after) - set(before))
+			for file in added:
+				if not self.on_added == None:
+					self.on_added(file)
+
+	def stop(self):
+		self.loop = 0
+
+this = DirectoryMonitor('C:\Users\Alex\Downloads',1)
 
 def ohman(thefile):
 	print 'Added: ' + thefile
+	this.stop()
+	this.directory = 'C:\\'
+	this.start()
 
 def ohman2(thefile):
 	print 'Removed: ' + thefile
 
-this = DirectoryMonitor('C:\Users\Alex\Downloads')
+
 this.on_added = ohman
 this.on_removed = ohman2
 this.start()
