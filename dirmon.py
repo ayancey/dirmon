c296691e59368a88dc22e35c1508b6eb9f46f55f
Alex Yancey <alexyancey3@gmail.com>
2015-02-07 02:17:51 -0800

    Fixed more silly stuff. I think decreased line count?

--- dirmon.py
+++ dirmon.py
@@ -4,10 +4,9 @@ import time
 class DirectoryMonitor:
 	directory = ''
 	interval = 0
-	loop = 0
 	on_added = None
 	on_removed = None
-	before = None
+	loop = False
 
 	def __init__(self, path, interval):
 		self.directory = path
@@ -15,28 +14,26 @@ class DirectoryMonitor:
 
 	def start(self):
 		self.loop = True
+
+		# The first before output is run outside of the loop
+		before = os.listdir(self.directory)
 		while self.loop:
-			# Polls the directory twice for the first iteration
-			if not self.before:
-				self.before = os.listdir(self.directory)
-				time.sleep(self.interval)
-				
+			time.sleep(self.interval)	
 			after = os.listdir(self.directory)
 
 			# Compares lists and passes file/folder names
-			removed = list(set(self.before) - set(after))
+			removed = list(set(before) - set(after))
 			for file in removed:
 				if self.on_removed:
 					self.on_removed(file)
 
-			added = list(set(after) - set(self.before))
+			added = list(set(after) - set(before))
 			for file in added:
 				if self.on_added:
 					self.on_added(file)
 
 			# Uses after output for the next before output
-			self.before = after
-			time.sleep(self.interval)
+			before = after
 
 	def stop(self):
 		self.loop = False
